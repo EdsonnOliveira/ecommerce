364e00221994c4d27f32149542b3bd2f5ef37ac3
edson.talent <edsonpinheiroliveira@gmail.com>
2024-07-11 11:25:15 -0300

    feat: imports atoms

---
+++ src/atomic/atoms/index.ts
@@ -0,0 +1,11 @@
+import BoxCommon from "./boxCommon";
+import Button from "./button";
+import Stamp from "./stamp";
+import Text from "./text";
+
+export {
+    BoxCommon,
+    Button,
+    Stamp,
+    Text,
+}
